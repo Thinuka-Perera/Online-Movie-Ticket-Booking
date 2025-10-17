130235fe1ce85f9ab80b6502e4cf5ee295d93b82
Thinuka-Perera <thinukapereraa@gmail.com>
2025-10-17 22:35:44 +0530

    Create footer and design the Movie page

--- client/src/components/TrailerSection.jsx
+++ client/src/components/TrailerSection.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 import { dummyTrailers } from "../assets/assets";
-import ReactPlayer from "react-player";
+import { PlayCircleIcon } from "lucide-react";
 import BlurCircle from "./BlurCircle";
 
 const TrailerSection = () => {
   const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
+
+  const getVideoId = (url) => {
+    const id = url.includes("v=")
+      ? url.split("v=")[1]
+      : url.split("youtu.be/")[1];
+    return id.split("&")[0];
+  };
 
   return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden">
       <p className="text-gray-300 font-medium text-lg max-w-[960px] mx-auto">
         Trailers
       </p>
+
       <div className="relative mt-6 max-w-[960px] mx-auto">
         <BlurCircle top="-100px" right="-100px" />
         <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
           <iframe
-            src={`https://www.youtube.com/embed/${
-              currentTrailer.videoUrl.split("v=")[1]
-            }?controls=0&modestbranding=1&showinfo=0&rel=0`}
+            src={`https://www.youtube.com/embed/${getVideoId(
+              currentTrailer.videoUrl
+            )}?controls=0&modestbranding=1&showinfo=0&rel=0`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
@@ -30,6 +38,26 @@ const TrailerSection = () => {
             }}
           />
         </div>
+      </div>
+
+      <div className="group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto">
+        {dummyTrailers.map((trailer) => (
+          <div
+            key={trailer.image}
+            className="relative hover:-translate-y-1 duration-300 transition cursor-pointer"
+            onClick={() => setCurrentTrailer(trailer)}
+          >
+            <img
+              src={trailer.image}
+              alt="trailer"
+              className="rounded-lg w-full h-full object-cover brightness-75"
+            />
+            <PlayCircleIcon
+              strokeWidth={1.6}
+              className="absolute top-1/2 left-1/2 w-8 h-8 md:w-12 md:h-12 transform -translate-x-1/2 -translate-y-1/2 text-white"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
